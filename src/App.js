a544f4a2b5ac52471615e1e3120c5019ef9c1042
HimmatToor <himmat14toor@gmail.com>
2025-12-06 19:14:14 -0800

    changed some code to make github pages compatible

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react'
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
+import {HashRouter as Router, Route, Routes} from "react-router-dom"
 import Home from './pages/Home'
 import ProjectReport from './pages/ProjectReport'
 import InputForm from './pages/InputForm'
